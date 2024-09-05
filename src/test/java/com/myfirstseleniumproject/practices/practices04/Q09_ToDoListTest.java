21afe27a858dc3ae39bccc4abaf4d069ec05bbfc
Zakir <zakirarslanov@hotmail.com>
2024-09-05 12:53:06 +0200

    Day18Practice

--- src/test/java/com/myfirstseleniumproject/practices/practices04/Q09_ToDoListTest.java
+++ src/test/java/com/myfirstseleniumproject/practices/practices04/Q09_ToDoListTest.java
@@ -8,6 +8,8 @@ public class Q09_ToDoListTest extends TestBase {
     public void toDoList(){
 
         // Go to http://webdriveruniversity.com/To-Do-List/index.html
+        driver.get("http://webdriveruniversity.com/To-Do-List/index.html");
+
         // Add todos: (Prepare breakfast, Wash the dishes, Take care of baby, Help your kid's homework, Study Selenium, Sleep)
         //  Strikethrough all todos.
         // Delete all todos.
